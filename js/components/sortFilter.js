a00fb86284c9e467aa596e42898fffd63587abed
Michael River Nilsson <99018848+RiverMichael@users.noreply.github.com>
2023-04-23 04:05:16 +0700

    added sort and filter function

--- js/components/sortFilter.js
+++ js/components/sortFilter.js
@@ -5,19 +5,36 @@ import { renderProducts } from "./render.js";
 const productsContainer = document.querySelector(".products");
 
 export async function sortProducts(event) {
-    let products = await getProducts();
+    const products = await getProducts();
+    let sortedProducts = products;
     const selected = event.target.value;
 
     if (selected === "price-low-high") {
-        products = products.sort((a, b) => parseFloat(a.prices.price) - parseFloat(b.prices.price));
+        sortedProducts = sortedProducts.sort((a, b) => parseFloat(a.prices.price) - parseFloat(b.prices.price));
     } else if (selected === "price-high-low") {
-        products = products.sort((a, b) => parseFloat(b.prices.price) - (a.prices.price));
+        sortedProducts = sortedProducts.sort((a, b) => parseFloat(b.prices.price) - (a.prices.price));
     } else if (selected === "name-az") {
-        products = products.sort((a, b) => a.name.localeCompare(b.name));
+        sortedProducts = sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
     } else if (selected === "name-za") {
-        products = products.sort((a, b) => b.name.localeCompare(a.name));
+        sortedProducts = sortedProducts.sort((a, b) => b.name.localeCompare(a.name));
     }
-
+    
+    
     clearHtml(productsContainer);
-    renderProducts(products, productsContainer);
+    renderProducts(sortedProducts, productsContainer);
+};
+
+export async function filterProducts(event) {
+    const products = await getProducts();
+    let filteredProducts = products;
+    let selected = event.target.value;
+
+    if (selected === "all") {
+        filteredProducts = products;
+    } else {
+        filteredProducts = filteredProducts.filter((product) => product.attributes[1].terms[0].name === selected)
+    };
+   
+    clearHtml(productsContainer);
+    renderProducts(filteredProducts, productsContainer);
 };
--- js/productsPage.js
+++ js/productsPage.js
@@ -1,13 +1,15 @@
 import { getProducts, renderProducts } from "./components/render.js";
 import { clearHtml } from "./components/createHtml.js";
 import { createMessage } from "./components/createMessage.js";
-import { sortProducts } from "./components/sortFilter.js";
+import { sortProducts, filterProducts } from "./components/sortFilter.js";
 
 const productsContainer = document.querySelector(".products");
 const messageContainer = document.querySelector(".message");
 const sortSelect = document.querySelector("#sort");
+const filterSelectColor = document.querySelector("#filter-color");
 
 sortSelect.addEventListener("change", sortProducts);
+filterSelectColor.addEventListener("change", filterProducts);
 
 async function productsPage() {
     const products = await getProducts();
